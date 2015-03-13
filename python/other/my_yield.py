a2cd8a464173e4822e64bdb94495e2f91587521a
leichongxiang <316666062@163.com>
2015-03-13 14:54:40 +0800

    move dir

---
+++ python/other/my_yield.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def func(n):
+	for i in range(n):
+		return i
+def func2(n):
+	for i in range(n):
+		yield i
+print func(3)
+f = func2(3)
+print f
+print f.next() 
+print f.next() 
